b36e97d3459402c99cfbf1f2b741f5563eb29cdd
Benjamin Seider <32752726+BSeider@users.noreply.github.com>
2019-08-04 15:58:52 -0400

    Add files via upload

--- lab4_cpu.cpp
+++ lab4_cpu.cpp
@@ -6,24 +6,27 @@
 using namespace std;
 #include <iterator>
 #include <algorithm>
+#include <random>
 #include <math.h>
 
 void valor_q(float iones_x[], float iones_y[], int cantidad){
     int a,b;
-    int Q_menor=100000000;
-    int carga=0;
+    float Q_menor=100000000.0;
+    float carga=0;
     int distancia;
     int menor[2];
-
+    float x_2,y_2;
     for(a=0; a<8192; a++){
         for(b=0; b<8192; b++){
             carga=0;
             for(int i = 0; i < cantidad; i++){
-                distancia = sqrt((a-iones_x[i])*(a-iones_x[i])+(b-iones_y[i])*(b-iones_y[i]));
+                x_2=(a-iones_x[i])*(a-iones_x[i]);
+                y_2=(b-iones_y[i])*(b-iones_y[i]);
+                distancia = sqrt(x_2+y_2);
                 if (distancia==0){
-                    distancia=0.00000000000000000000000000000001;
+                    distancia=0.0000000000001;
                 }
-                carga+=1/distancia;
+                carga+=1.0/distancia;
             }
             if(carga < Q_menor){
                 Q_menor=carga;
@@ -37,22 +40,26 @@ void valor_q(float iones_x[], float iones_y[], int cantidad){
 }
 
 int main(int argc, char const *argv[]){
-    srand48(time(NULL));
-    float *iones_x[6000], *iones_y[6000];
+    float *iones_x, *iones_y;
     float x, y;
-    
+
+    iones_x = new float[6000];
+	iones_y = new float[6000];
+
+    std::random_device rd;
+    std::default_random_engine generator(rd()); // rd() provides a random seed
+    std::uniform_real_distribution<double> distribution(0.1,8192);
+
     for (int i = 0; i < 5000; ++i){
 
-        x = drand48()*8192;
-        y = drand48()*8192;
-
-        *iones_x[i]=x;
-        *iones_y[i]=y;
-
+        x = distribution(generator);
+        y = distribution(generator);
+        iones_x[i]=x;
+        iones_y[i]=y;
 	}
     int cantidad;
-    for(cantidad=5000; cantidad < 1000; cantidad++){
-        valor_q(*iones_x, *iones_y, cantidad);
+    for(cantidad=5000; cantidad < 6000; cantidad++){
+        valor_q(iones_x, iones_y, cantidad);
     }
 
 
